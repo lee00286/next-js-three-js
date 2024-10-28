4fca499b35726d9d008c001b31fe92a14889d1ef
lee00286 <lee00286@naver.com>
2024-10-28 15:01:45 +0900

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
+    screens: {
+      xxl: { max: '1440px' },
+      xl: { max: '1280px' },
+      lg: { max: '1024px' },
+      xxmd: { max: '960px' },
+      xmd: { max: '854px' },
+      md: { max: '768px' },
+      xsm: { max: '640px' },
+      sm: { max: '480px' },
+      xs: { max: '375px' },
+      xxs: { max: '310px' }
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
